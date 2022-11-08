74b654c1b44bb75891f58596794b9ada46c5943d
liubin <everyhook@live.cn>
2022-11-08 12:01:05 +0800

    MODIFY FILE

--- build.sh
+++ build.sh
@@ -1,39 +1,33 @@
 docker image ls|grep none|awk '{print $3}'|xargs docker image remove
+mvn clean package
 cwd=$(pwd)
 
-cd eureka
-# mvn clean package
+cd $cwd/eureka || return
 docker build -t eureka .
 kubectl delete -f eureka-deploy.yml
 minikube image remove eureka
 minikube image load eureka:latest
 kubectl apply -f eureka-deploy.yml
 
-cd $cwd
-cd feign-eureka/client
-# mvn clean package
+cd $cwd/client || return
 docker build -t helloclient .
 kubectl delete -f client-deploy.yml
 minikube image remove helloclient
 minikube image load helloclient:latest
 kubectl apply -f client-deploy.yml
 
-cd $cwd
-cd feign-eureka/server
-# mvn clean package
+cd $cwd/server || return
 docker build -t helloserver .
 kubectl delete -f server-deploy.yml
 minikube image remove helloserver
 minikube image load helloserver:latest
 kubectl apply -f server-deploy.yml
 
-cd $cwd
-cd feign-eureka/gateway
-# mvn clean package
+cd $cwd/gateway || return
 docker build -t gateway .
-kubectl delete -f k8s/deployment.yml
+kubectl delete -f k8s/deployment.yaml
 minikube image remove gateway
 minikube image load gateway:latest
-kubectl apply -f k8s/deployment.yml
+kubectl apply -f k8s/deployment.yaml
 
 cd $cwd
--- delete.sh
+++ delete.sh
@@ -1,18 +1,20 @@
 docker image ls|grep none|awk '{print $3}'|xargs docker image rm
 cwd=$(pwd)
 
-cd eureka
+cd $cwd/eureka || return
 kubectl delete -f eureka-deploy.yml
 minikube image remove eureka
 
-cd $cwd
-cd feign-eureka/client
+cd $cwd/client || return
 kubectl delete -f client-deploy.yml
 minikube image remove helloclient
 
-cd $cwd
-cd feign-eureka/server
+cd $cwd/server || return
 kubectl delete -f server-deploy.yml
 minikube image remove helloserver
+
+cd $cwd/gateway || return
+kubectl delete -f k8s/deployment.yaml
+minikube image remove gateway
 
 cd $cwd
